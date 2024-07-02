8fb66892f8494fd18cfc4c827de08ce7250bdb40
Chester J. Ayala <chesterayala.ca@gmail.com>
2024-07-02 12:27:34 +0800

    Rename placeworkers for clarity

--- Source/RimTerra/PlaceWorkers/PlaceWorker_RichSoil.cs
+++ Source/RimTerra/PlaceWorkers/PlaceWorker_RichSoil.cs
@@ -3,7 +3,7 @@ using Verse;
 
 namespace RimTerra.PlaceWorkers
 {
-    public class PlaceWorker_OnSoil : PlaceWorker
+    public class PlaceWorker_RichSoil : PlaceWorker
     {
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
--- Source/RimTerra/PlaceWorkers/PlaceWorker_Soil.cs
+++ Source/RimTerra/PlaceWorkers/PlaceWorker_Soil.cs
@@ -3,7 +3,7 @@ using Verse;
 
 namespace RimTerra.PlaceWorkers
 {
-    public class PlaceWorker_OnStonySoil : PlaceWorker
+    public class PlaceWorker_Soil : PlaceWorker
     {
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
